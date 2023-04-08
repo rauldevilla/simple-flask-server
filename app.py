4d131949ca845a4ec8c6a0d54fca06bf2ca7ed46
rauldevilla <raul.devilla@gmail.com>
2023-04-08 10:42:02 -0500

    Create very long response

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
+def __get_very_long_response__():
+    response = []
+    for index in range(10):
+        with open('data.json', 'r') as f:
+            response.append(json.load(f))
+    return response
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+@app.route('/delay')
+def delay():
+    return __get_very_long_response__()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
